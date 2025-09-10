edbbf669045a2288f9076249a2c50fdebf35e38c
hoangquyen <hoangquyen97st@gmail.com>
2025-09-10 21:27:59 +0700

    Tao Project Git lan dau

---
+++ src/Buoi_1.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Buoi_1 {
+    public static void main(String[] args) {
+       int i=0;
+       while (i<=5){
+           system.out.print("Lan thu: "+i);
+           i++;
+       }
+    }
+}
