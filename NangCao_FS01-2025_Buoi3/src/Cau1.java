4d5df63f55961510decd22b1f4b23ec61d72cd24
hoangquyen <hoangquyen97st@gmail.com>
2025-09-13 16:50:48 +0700

    NangCao_FS01-2025_Buoi3

---
+++ NangCao_FS01-2025_Buoi3/src/Cau1.java
@@ -0,0 +1,10 @@
+
+public class Cau1 { // khai báo lớp chính
+    public static void main(String[] args) { // phương thức main — điểm bắt đầu của chương trình
+        //Dùng for để in ra màn hình các số từ 1 → 100
+        System.out.println("In các số từ 1 đến 100");
+        for (int i = 1; i <= 100; i++) { // khởi tạo i=1; điều kiện i<=100; sau mỗi vòng tăng i lên 1
+            System.out.print(i + " "); // in giá trị i và thêm dấu cách
+        }
+    }
+}
